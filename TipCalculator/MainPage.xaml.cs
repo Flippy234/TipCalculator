e4418459fd09f0bcd8d401a3082759dd4cae2cfa
Tyler Hanson <700211854@fvtc.edu>
2026-01-27 01:09:12 -0600

    added tip calculator / still WIP

--- TipCalculator/MainPage.xaml.cs
+++ TipCalculator/MainPage.xaml.cs
@@ -10,27 +10,41 @@ public partial class MainPage : ContentPage
 
     private void FifteenPercentTip_OnClicked(object sender, EventArgs e)
     {
+        
         //Work on this
         //Multiply bill amount by percent tip
-        double dblBillAmount, dblTipAmount, dblTotalAmount;
+        double dblBillAmount;
         Double.TryParse(txtBillAmount.Text, out dblBillAmount);
-        //Double.TryParse()
         double tipRate = 0.15;
-        dblTipAmount = dblBillAmount * tipRate;
-        //dblTotalAmount = dblBillAmount + dblTipAmount;
+        double dblTipAmount = dblBillAmount * tipRate;
+        double dblTotalAmount = dblBillAmount + dblTipAmount;
 
-        lblTipAmount.Text = lblTip1.Text + "" + "$" + dblTipAmount;
-        
+        lblTipAmount.Text = $"15% Tip: ${dblTipAmount}";
+        lblTotalAmount.Text = $"${dblTotalAmount}";
     }
 
     private void TwentyPercentTip_OnClicked(object sender, EventArgs e)
     {
-       
+        double dblBillAmount;
+        Double.TryParse(txtBillAmount.Text, out dblBillAmount);
+        double tipRate = 0.20;
+        double dblTipAmount = dblBillAmount * tipRate;
+        double dblTotalAmount = dblBillAmount + dblTipAmount;
+
+        lblTipAmount.Text = $"20% Tip: ${dblTipAmount}";
+        lblTotalAmount.Text = $"${dblTotalAmount}";
     }
 
     private void TwentyFivePercentTip_OnClicked(object sender, EventArgs e)
     {
-        
+        double dblBillAmount;
+        Double.TryParse(txtBillAmount.Text, out dblBillAmount);
+        double tipRate = 0.25;
+        double dblTipAmount = dblBillAmount * tipRate;
+        double dblTotalAmount = dblBillAmount + dblTipAmount;
+
+        lblTipAmount.Text = $"25% Tip: ${dblTipAmount}";
+        lblTotalAmount.Text = $"${dblTotalAmount}";
     }
     
 }
