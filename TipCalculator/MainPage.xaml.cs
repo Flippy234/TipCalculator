8a182e4e9962952c345b88525d9bbbf47b20165a
Tyler Hanson <700211854@fvtc.edu>
2026-01-26 23:28:45 -0600

    TipCalculator Draft

---
+++ TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace TipCalculator;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        Title = "Tip Calculator";
+    }
+
+    private void FifteenPercentTip_OnClicked(object sender, EventArgs e)
+    {
+        //Multiply bill amount by percent tip
+        double dblBillAmount, dblTipAmount, dblTotalAmount;
+        Double.TryParse(txtBillAmount.Text, out dblBillAmount);
+        //Double.TryParse()
+        double tipRate = 0.15;
+        dblTipAmount = dblBillAmount * tipRate;
+        //dblTotalAmount = dblBillAmount + dblTipAmount;
+
+        lblTipAmount.Text = lblTip1.Text + "" + "$" + dblTipAmount;
+        
+    }
+
+    private void TwentyPercentTip_OnClicked(object sender, EventArgs e)
+    {
+       
+    }
+
+    private void TwentyFivePercentTip_OnClicked(object sender, EventArgs e)
+    {
+        
+    }
+    
+}
